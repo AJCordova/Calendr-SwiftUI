9d3e0bce6a993d43cf2c2005c5b75df47ece2b87
AJCordova <archonist252@gmail.com>
2025-06-13 17:13:10 +0100

    Initial project commit.

---
+++ Calendr-SwiftUI/Calendr-SwiftUI/Calendr_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendr_SwiftUIApp.swift
+//  Calendr-SwiftUI
+//
+//  Created by Jireh Cordova on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Calendr_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
